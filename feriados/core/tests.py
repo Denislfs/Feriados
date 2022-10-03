c5799bc1538c5618a2edb4fca83532f234c90c75
Denislfs <denislfs@hotmail.com>
2022-10-02 22:35:49 -0300

    Complemento com o Desafio

--- feriados/core/tests.py
+++ feriados/core/tests.py
@@ -1,20 +1,40 @@
 from django.test import TestCase
-
-class NatalTest(TestCase):
- def setUp(self):
-    self.resp = self.client.get('/')
-
- def test_200_response(self):
-    self.assertEqual(200, self.resp.status_code)
-
- def test_texto(self):
-    self.assertContains(self.resp, 'natal')
-    
- def test_template_natal(self):
-   self.assertTemplateUsed(self.resp, 'natal.html')
-
 from core.models import FeriadoModel
 from datetime import datetime
+
+class SemFeriadoTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_texto(self):
+        self.assertContains(self.resp, 'Lamento')
+        self.assertContains(self.resp, 'hoje não tem feriado.')
+
+    def test_template_natal(self):
+        self.assertTemplateUsed(self.resp, 'feriado.html')
+
+class PossuiFeriadoTest(TestCase):
+    def setUp(self):
+        hoje = datetime.today()
+        feriado = FeriadoModel(nome='Dia do Saci', dia=hoje.day, mes=hoje.month)
+        feriado.save()
+        self.resp = self.client.get('/')
+
+    def test_dados_no_banco_fake(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_texto(self):
+        self.assertContains(self.resp, 'Dia do Saci')
+
+    def test_template_natal(self):
+        self.assertTemplateUsed(self.resp, 'feriado.html')
+
 
 class FeriadoModelTest(TestCase):
     def setUp(self):
